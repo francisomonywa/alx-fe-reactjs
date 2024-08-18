e158c52b7d5073a12ef29069dc997a7bf45e7945
Francis <francisomonywa37@gmail.com>
2024-08-18 21:45:41 +0300

    Task: Apply Inline CSS Styling to React Components

--- alx-react-app-new/components/UserProfile.jsx
+++ alx-react-app-new/components/UserProfile.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
-
 function UserProfile(props) {
-    return (
-      <div>
-         <h2>{props.name}</h2>
-         <p>Age: {props.age}</p>
-         <p>Bio: {props.bio}</p>
-       </div>
-    )
-  }
-  
-  export default UserProfile
+  return (
+    <div style={{
+      border: "1px solid gray",
+      padding: "10px",
+      margin: "10px"
+    }}>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <h2 style={{color: "blue"}}>{props.name}</h2>
+      <p style={{fontWeight: "bold"}}>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  )
+}
+export default UserProfile
