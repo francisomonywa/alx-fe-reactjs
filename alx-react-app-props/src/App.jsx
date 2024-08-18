edae13d37d60ea432606ede15a1588203f558a56
Francis <francisomonywa37@gmail.com>
2024-08-18 23:13:20 +0300

    Rename

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,29 +1,13 @@
-// import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import ProfilePage from './components/ProfilePage';
+import ProfilePage from './components/UserProfile';
+import UserContext from './components/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
   return (
-    <>
-      <Header />
-      <WelcomeMessage />
-      <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Footer />
-    </>
-  )
     <UserContext.Provider value={userData}>
       <ProfilePage userData={userData} />
     </UserContext.Provider>
   );
 }
-
-export default App
 export default App;
