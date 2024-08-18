8855493ebb371aca2e0004753f54f8f2b46fe5e9
Francis <francisomonywa37@gmail.com>
2024-08-18 21:55:40 +0300

    Task: Create a Simple Counter Application Using State and Hooks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,10 +1,9 @@
-import './App.css'
-// import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import { Counter } from './components/Counter'
 
 function App() {
 
@@ -13,6 +12,7 @@ function App() {
       <Header />
       <WelcomeMessage />
       <MainContent />
+      <Counter />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
