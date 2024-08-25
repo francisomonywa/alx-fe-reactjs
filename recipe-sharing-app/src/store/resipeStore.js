ac01dc33bed2e22f579ad5f58d79073b3ca8d869
Francis <francisomonywa37@gmail.com>
2024-08-25 22:19:32 +0300

    Add 1

--- recipe-sharing-app/src/store/resipeStore.js
+++ recipe-sharing-app/src/store/resipeStore.js
@@ -1,9 +1,24 @@
-import { create } from 'zustand'
+ import { create } from "zustand";
 
-const useRecipeStore = create(set => ({
-    recipes: [],
-    addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-    setRecipes: (recipes) => set({ recipes })
-}));
 
-export default useRecipeStore;
+ const useRecipeStore = create((set) => ({
+     recipes: [],
+
+     addRecipe: (newRecipe) =>
+         set((state) => ({
+             recipes: [...state.recipes, newRecipe],
+         })),
+     deleteRecipe: (id) =>
+         set((state) => ({
+             recipes: state.recipes.filter((recipe) => recipe.id !== id),
+         })),
+     updateRecipe: (updatedRecipe) =>
+         set((state) => ({
+             recipes: state.recipes.map((recipe) =>
+                 recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+             ),
+         })),
+     setRecipes: (recipes) => set({ recipes }),
+ }));
+
+ export default useRecipeStore;
