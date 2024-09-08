ab54420adf8242050c625b03df1d535593e77d71
Francis <francisomonywa37@gmail.com>
2024-09-08 21:42:20 +0300

    Tailwind CSS purge configuration

--- tailwind-react-integration/Tailwind.config.js
+++ tailwind-react-integration/Tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
+
+    purge: ["./src/**/*.{js,jsx,ts,tsx}",
+        "./public/index.html"
     ],
-    theme: {
-        extend: {},
-    },
+    darkMode: false,
+    theme: { extend: {} },
+    variants: { extend: {} },
     plugins: [],
-}
+
+};
