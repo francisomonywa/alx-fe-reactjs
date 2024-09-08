e001f5fc2c50c4724d2e35d16ce7a558508922aa
Francis <francisomonywa37@gmail.com>
2024-09-08 21:47:31 +0300

    Styling a Provided React Component with Tailwind CSS

--- tailwind-react-integration/Tailwind.config.js
+++ tailwind-react-integration/Tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 
-    purge: ["./src/**/*.{js,jsx,ts,tsx}",
-        "./public/index.html"
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
     ],
-    darkMode: false,
-    theme: { extend: {} },
-    variants: { extend: {} },
+    theme: {
+        extend: {},
+    },
     plugins: [],
 
-};
+}
--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,10 +1,14 @@
+
+import UserProfile from "./components/UserProfile"
+
 function App() {
 
-    return (
-      <div>
-        <h1 className="text-3xl font-bold">Setting Up Tailwind CSS in a New React Project</h1>
-      </div>
-    )
-  }
-  
-  export default App
+  return (
+    <div>
+      
+      <h1 className="text-3xl font-bold">Styling a Provided React Component with Tailwind CSS</h1>
+      <UserProfile />
+    </div>
+  )
+}
+export default App
