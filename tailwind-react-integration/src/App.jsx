aea0ddf17ad385d7afc56d80c33fdc3c585150fc
Francis <francisomonywa37@gmail.com>
2024-09-08 21:56:24 +0300

    Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,12 +1,10 @@
-
 import UserProfile from "./components/UserProfile"
-
 function App() {
 
   return (
     <div>
-      
-      <h1 className="text-3xl font-bold">Styling a Provided React Component with Tailwind CSS</h1>
+       
+      <h1 className="text-3xl font-bold">Responsive Design Implementation Using Tailwind CSS</h1>
       <UserProfile />
     </div>
   )
