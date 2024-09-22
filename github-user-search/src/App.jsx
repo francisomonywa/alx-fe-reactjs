c6679a3bcfe049da060b0ac34062ced775a4a9db
Francis <francisomonywa37@gmail.com>
2024-09-23 01:40:04 +0300

    Setting Up a React Project for GitHub User Search Application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className='text-3xl font-bold text-center'>GitHub User Search Application</h1>
+    </div>
+  );
+};
+export default App;
